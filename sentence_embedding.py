a256f13b96553718d1c0cce7a0354898794de46d
CJ <chenjian900928@gmail.com>
2018-04-28 09:52:21 +0800

    change wv to word_vecs

--- sentence_embedding.py
+++ sentence_embedding.py
@@ -1,18 +1,18 @@
-def global_metric(word_vec):
-    cov = np.cov(word_vec.T)
-    average = np.mean(word_vec, axis=0)
+def global_metric(word_vecs):
+    cov = np.cov(word_vecs.T)
+    average = np.mean(word_vecs, axis=0)
     return np.linalg.inv(cov), average
 
 
 def metric_distance(inverse_cov, vec1, vec2):
     return math.sqrt(np.matmul(vec1, inverse_cov).dot(vec2))
 
-def metric_emb(wv, sens, inverse_cov, global_avg, global_only):
+def metric_emb(word_vecs, sens, inverse_cov, global_avg, global_only):
     n_sen = len(sens)
-    emb = np.empty((n_sen, wv.shape[1]))
+    emb = np.empty((n_sen, word_vecs.shape[1]))
     for idx, sen in enumerate(sens):
         if len(sen) > 0:
-            raw_emb = wv[sen, :]
+            raw_emb = word_vecs[sen, :]
             if global_only:
                 distance = np.array([metric_distance(inverse_cov, global_avg, vec) for vec in raw_emb])
             else:
@@ -23,28 +23,28 @@ def metric_emb(wv, sens, inverse_cov, global_avg, global_only):
             print(weights)
             emb[idx] = weights.dot(raw_emb)
         else:
-            emb[idx] = np.zeros(wv.shape[1])
+            emb[idx] = np.zeros(word_vecs.shape[1])
 
     return torch.from_numpy(emb)
 
-def average_emb(wv, sens):
+def average_emb(word_vecs, sens):
     n_sen = len(sens)
-    emb = np.empty((n_sen, wv.shape[1]))
+    emb = np.empty((n_sen, word_vecs.shape[1]))
     for idx, sen in enumerate(sens):
         if len(sen) > 0:
-            emb[idx] = np.sum(wv[sen, :], axis=0) / len(sen)
+            emb[idx] = np.sum(word_vecs[sen, :], axis=0) / len(sen)
         else:
-            emb[idx] = np.zeros(wv.shape[1])
+            emb[idx] = np.zeros(word_vecs.shape[1])
     return torch.from_numpy(emb)
 
-def weighted_emb(wv, sens, weights):
+def weighted_emb(word_vecs, sens, weights):
     n_sen = len(sens)
-    emb = np.empty((n_sen, wv.shape[1]), dtype=np.float32)
+    emb = np.empty((n_sen, word_vecs.shape[1]), dtype=np.float32)
     for idx, sen in enumerate(sens):
         if len(sen) > 0:
-            emb[idx] = weights[sen].dot(wv[sen, :]) / len(sen)
+            emb[idx] = weights[sen].dot(word_vecs[sen, :]) / len(sen)
         else:
-            emb[idx] = np.zeros(wv.shape[1])
+            emb[idx] = np.zeros(word_vecs.shape[1])
     return torch.from_numpy(emb)
     
 def first_pca(sen_vecs):
