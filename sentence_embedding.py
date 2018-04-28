d05c7bf10197d3f507e2c414bd7bfca9882c8579
CJ <chenjian900928@gmail.com>
2018-04-28 11:00:05 +0800

    replace pytorch by numpy

--- sentence_embedding.py
+++ sentence_embedding.py
@@ -1,16 +1,69 @@
-import numpy as np 
-import torch
+def average_emb(word_vecs, sens):
+    """
+    Compute the sentence weighted average vectors
+    :param word_vecs: wv[i,:] - vector of word i - V*D
+    :param sens: sens[i] - word indices list of sentence i - N * Unknown
+    :return: emb[i, :] - embedding of sentence i - N * V
+    """
+    n_sen = len(sens)
+    emb = np.empty((n_sen, word_vecs.shape[1]))
+    for idx, sen in enumerate(sens):
+        if len(sen) > 0:
+            emb[idx] = np.sum(word_vecs[sen, :], axis=0) / len(sen)
+        else:
+            emb[idx] = np.zeros(word_vecs.shape[1])
+    return emb
 
-def global_metric(word_vecs):
-    cov = np.cov(word_vecs.T)
-    average = np.mean(word_vecs, axis=0)
+
+def weighted_emb(word_vecs, sens, weights):
+    """
+    Compute the sentence weighted average vectors
+    :param word_vecs: wv[i,:] - vector of word i - V*D
+    :param sens: sens[i] - word indices list of sentence i - N * Unknown
+    :param weights: weights[i] - weight of word i - V * 1
+    :return: emb[i, :] - embedding of sentence i - N * V
+    """
+    n_sen = len(sens)
+    emb = np.empty((n_sen, word_vecs.shape[1]), dtype=np.float32)
+    for idx, sen in enumerate(sens):
+        if len(sen) > 0:
+            emb[idx] = weights[sen].dot(word_vecs[sen, :]) / len(sen)
+        else:
+            emb[idx] = np.zeros(word_vecs.shape[1])
+    return emb
+
+
+def first_pca(sen_vecs):
+    """
+    :param sen_vecs: sentence embeddings, no need normalization 
+    :return: first component computed from pca
+    """
+    U, S, V = np.linalg.svd(sen_vecs)
+    return V[:, 0:1]
+
+
+def sif_emb(sen_vecs, first_component):
+    """
+    first component
+    :param sen_vecs: sentence embeddings  - N * V
+    :param first_component: compute by pca - Ｖ * 1
+    :return: new sentence embeddings - N * V
+    """
+    emb = sen_vecs - (sen_vecs.dot(first_component)).dot(first_component.T)
+    return emb
+
+
+def global_metric(word_vec):
+    cov = np.cov(word_vec.T)
+    average = np.mean(word_vec, axis=0)
     return np.linalg.inv(cov), average
 
 
 def metric_distance(inverse_cov, vec1, vec2):
     return math.sqrt(np.matmul(vec1, inverse_cov).dot(vec2))
 
-def metric_emb(word_vecs, sens, inverse_cov, global_avg, global_only):
+
+def metric_embedding(word_vecs, sens, inverse_cov, global_avg, global_only):
     n_sen = len(sens)
     emb = np.empty((n_sen, word_vecs.shape[1]))
     for idx, sen in enumerate(sens):
@@ -23,36 +76,13 @@ def metric_emb(word_vecs, sens, inverse_cov, global_avg, global_only):
                 distance = np.array([metric_distance(inverse_cov, avg_emb, vec) for vec in raw_emb])
             avg_distance = distance/(2*np.average(distance))
             weights = np.array([1.8*(x - 0.5) + 0.5 for x in avg_distance])
-            print(weights)
             emb[idx] = weights.dot(raw_emb)
         else:
             emb[idx] = np.zeros(word_vecs.shape[1])
-    return torch.from_numpy(emb)
 
-def average_emb(word_vecs, sens):
-    n_sen = len(sens)
-    emb = np.empty((n_sen, word_vecs.shape[1]))
-    for idx, sen in enumerate(sens):
-        if len(sen) > 0:
-            emb[idx] = np.sum(word_vecs[sen, :], axis=0) / len(sen)
-        else:
-            emb[idx] = np.zeros(word_vecs.shape[1])
-    return torch.from_numpy(emb)
+    return emb
 
-def weighted_emb(word_vecs, sens, weights):
-    n_sen = len(sens)
-    emb = np.empty((n_sen, word_vecs.shape[1]), dtype=np.float32)
-    for idx, sen in enumerate(sens):
-        if len(sen) > 0:
-            emb[idx] = weights[sen].dot(word_vecs[sen, :]) / len(sen)
-        else:
-            emb[idx] = np.zeros(word_vecs.shape[1])
-    return torch.from_numpy(emb)
-    
-def first_pca(sen_vecs):
-    U, S, V = torch.svd(sen_vecs)
-    return torch.unsqueeze(V[0], 1)
 
-def sif(sen_vecs, first_component):
-    sif_embeddings = sen_vecs - sen_vecs.mm(first_component).mm(first_component.t())
-    return sif_embeddings
+wvs = np.random.randn(20, 5)
+sens = [0, 4, 6, 11]
+sens_vec = average_emb(wvs, sens)
